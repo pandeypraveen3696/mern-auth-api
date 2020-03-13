5d68cea13d6b54adf8ac49597acc28f6b24fa498
Praveen Kumar Pandey <56410417+pandeypraveen3696@users.noreply.github.com>
2020-03-14 00:14:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,12 +18,18 @@ mongoose
   .then(() => console.log("DB CONNECTED"))
   .catch(err => console.log("DB ERROR : ", err));
 
-
-app.all('*', function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-   next();
+// extra code for heroku
+app.use(function(req, res, next) {
+res.header(“Access-Control-Allow-Origin”, “*”);
+res.header(“Access-Control-Allow-Methods”, “GET,PUT,POST,DELETE”);
+res.header(
+“Access-Control-Allow-Headers”,
+“Origin, X-Requested-With, Content-Type, Accept”
+);
+next();
 });
+app.options(“*”, cors());
+// end of extra code for heroku
 
 app.use(express.static(environmentRoot + '/public'));
 
